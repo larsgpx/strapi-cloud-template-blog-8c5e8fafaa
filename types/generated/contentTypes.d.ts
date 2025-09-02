8bc3adc9a459252015090f8741300b44116c8606
Edgar Cardona <edgarcardona87@gmail.com>
2025-09-01 23:17:58 -0400

    FEAT: User entity with phoneNumber field

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1023,7 +1023,6 @@ export interface PluginUsersPermissionsUser
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -1048,6 +1047,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    phoneNumber: Schema.Attribute.String;
     provider: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     resetPasswordToken: Schema.Attribute.String & Schema.Attribute.Private;
