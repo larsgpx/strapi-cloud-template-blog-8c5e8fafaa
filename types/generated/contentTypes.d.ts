63c0af3195936db28ddd34baeca3e7befc427da4
Larry Garcia <larsgpx@gmail.com>
2025-09-17 22:12:37 -0500

    new schemas to subcategories

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -484,6 +484,7 @@ export interface ApiProfessionalSubCategoryProfessionalSubCategory
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    icon: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
